1b28ed7daf8e4d1403062ec675ad09845ea96fc2
liyuandan <lilylcn13@gmail.com>
2018-02-19 23:27:15 -0500

    added search at four corners of the search area

---
+++ lab5-1.5/src/ca/mcgill/ecse211/lab5/Search.java
@@ -0,0 +1,309 @@
+package ca.mcgill.ecse211.lab5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import ca.mcgill.ecse211.odometer.Odometer;
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
+
+public class Search {
+	private EV3LargeRegulatedMotor leftMotor;
+	private EV3LargeRegulatedMotor rightMotor;
+	private EV3UltrasonicSensor usSensor;
+	private Odometer odometer;
+	private Navigation navigation;
+	private EV3ColorSensor cSensor;
+	private static SampleProvider sampleProvider;
+	private static int sampleSize;
+	private static int thresholdRed = 10;
+	private static double redMeanR = 14.3;
+	private static double redMeanG = 2.146;
+	private static double redMeanB = 1.626;
+	private static double blueMeanR = 2.34;
+	private static double blueMeanG = 5.82;
+	private static double blueMeanB = 7.4;
+	private static double yellowMeanR = 37.3;
+	private static double yellowMeanG = 21.7;
+	private static double yellowMeanB = 3.85;
+	private static double whiteMeanR = 21.9;
+	private static double whiteMeanG = 21.68;
+	private static double whiteMeanB = 15.56;
+	
+
+	public Search(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor, Odometer odometer, 
+			EV3UltrasonicSensor usSensor) {
+		this.leftMotor = leftMotor;
+		this.rightMotor = rightMotor;
+		this.odometer = odometer;
+		this.usSensor = usSensor;
+		this.navigation = new Navigation(leftMotor, rightMotor);
+		this.cSensor = new EV3ColorSensor(LocalEV3.get().getPort("S4"));
+	}
+	
+	public void search(double LLx, double LLy, double URx, double URy, int TB) {	
+		//first point
+		double angle=0;
+		boolean notFinished = true;
+		navigation.turnTo(0);
+		leftMotor.stop(true);
+		rightMotor.stop();
+		Sound.beep();
+		while(notFinished) {
+			leftMotor.setSpeed(80);
+			rightMotor.setSpeed(80);
+			leftMotor.setAcceleration(500);
+			rightMotor.setAcceleration(500);
+			leftMotor.forward();
+			rightMotor.backward();
+			while(leftMotor.isMoving()) {
+				//double distance = getDistance();
+				double dist = getDistance();
+				if(dist< 60) {
+					leftMotor.stop(true);
+					rightMotor.stop();
+					Sound.beepSequenceUp();
+					angle = odometer.getT();
+					findObject(dist, TB);
+                    leftMotor.setSpeed(150);
+                    rightMotor.setSpeed(150);
+                    leftMotor.setAcceleration(500);
+                    rightMotor.setAcceleration(500);
+					leftMotor.rotate(convertDistance(Lab5.WHEEL_RAD, -dist), true);
+					rightMotor.rotate(convertDistance(Lab5.WHEEL_RAD, -dist), false);
+					//navigation.travelTo(LLx, LLy);
+					navigation.turnTo(angle+10);
+				}
+				if(Math.abs(odometer.getT() - 90) <= 8) {
+					leftMotor.stop(true);
+					rightMotor.stop();
+					notFinished = false;
+				}
+			}	
+		}
+        notFinished=true;
+        navigation.travelTo(URx,LLy);
+        navigation.turnTo(0);
+        while(notFinished) {
+            //leftMotor.rotate(convertAngle(Lab5.WHEEL_RAD, Lab5.TRACK, angle-270), true);
+            //rightMotor.rotate(-convertAngle(Lab5.WHEEL_RAD, Lab5.TRACK, angle-270), true);
+            leftMotor.setSpeed(80);
+            rightMotor.setSpeed(80);
+            leftMotor.setAcceleration(500);
+            rightMotor.setAcceleration(500);
+            leftMotor.backward();
+            rightMotor.forward();
+            while(leftMotor.isMoving()) {
+                //double distance = getDistance();
+                double dist = getDistance();
+                if(dist< 60) {
+                    leftMotor.stop(true);
+                    rightMotor.stop();
+                    Sound.beepSequenceUp();
+                    angle = odometer.getT();
+                    findObject(dist, TB);
+                    leftMotor.setSpeed(150);
+                    rightMotor.setSpeed(150);
+                    leftMotor.setAcceleration(500);
+                    rightMotor.setAcceleration(500);
+                    leftMotor.rotate(convertDistance(Lab5.WHEEL_RAD, -dist), true);
+                    rightMotor.rotate(convertDistance(Lab5.WHEEL_RAD, -dist), false);
+                    //navigation.travelTo(LLx, LLy);
+                    navigation.turnTo(angle-10);
+                }
+            }
+            if(Math.abs(odometer.getT() -270) <=8) {
+                leftMotor.stop(true);
+                rightMotor.stop();
+                notFinished = false;
+            }
+        }
+        notFinished=true;
+        navigation.travelTo(URx,URy);
+        navigation.turnTo(180);
+        while(notFinished) {
+            //leftMotor.rotate(convertAngle(Lab5.WHEEL_RAD, Lab5.TRACK, 90), true);
+            //rightMotor.rotate(-convertAngle(Lab5.WHEEL_RAD, Lab5.TRACK, 90), true);
+            leftMotor.setSpeed(80);
+            rightMotor.setSpeed(80);
+            leftMotor.setAcceleration(500);
+            rightMotor.setAcceleration(500);
+            leftMotor.forward();
+            rightMotor.backward();
+            while(leftMotor.isMoving()) {
+                //double distance = getDistance();
+                double dist = getDistance();
+                if(dist< 60) {
+                    leftMotor.stop(true);
+                    rightMotor.stop();
+                    Sound.beepSequenceUp();
+                    angle = odometer.getT();
+                    findObject(dist, TB);
+                    leftMotor.setSpeed(150);
+                    rightMotor.setSpeed(150);
+                    leftMotor.setAcceleration(500);
+                    rightMotor.setAcceleration(500);
+                    leftMotor.rotate(convertDistance(Lab5.WHEEL_RAD, -dist), true);
+                    rightMotor.rotate(convertDistance(Lab5.WHEEL_RAD, -dist), false);
+                    //navigation.travelTo(LLx, LLy);
+                    navigation.turnTo(angle-10);
+                }
+            }
+            if(Math.abs(odometer.getT()-270) <=8) {
+                leftMotor.stop(true);
+                rightMotor.stop();
+                notFinished = false;
+            }
+        }
+        notFinished=true;
+        navigation.travelTo(LLx * Lab5.TILE_SIZE,URy * Lab5.TILE_SIZE);
+        navigation.turnTo(180);
+        while(notFinished) {
+            //leftMotor.rotate(convertAngle(Lab5.WHEEL_RAD, Lab5.TRACK, -90), true);
+            //rightMotor.rotate(-convertAngle(Lab5.WHEEL_RAD, Lab5.TRACK, -90), true);
+            leftMotor.setSpeed(80);
+            rightMotor.setSpeed(80);
+            leftMotor.setAcceleration(500);
+            rightMotor.setAcceleration(500);
+            leftMotor.backward();
+            rightMotor.forward();
+            while(leftMotor.isMoving()) {
+                //double distance = getDistance();
+                double dist = getDistance();
+                if(dist< 60) {
+                    leftMotor.stop(true);
+                    rightMotor.stop();
+                    Sound.beepSequenceUp();
+                    angle = odometer.getT();
+                    findObject(dist, TB);
+                    leftMotor.setSpeed(150);
+                    rightMotor.setSpeed(150);
+                    leftMotor.setAcceleration(500);
+                    rightMotor.setAcceleration(500);
+                    leftMotor.rotate(convertDistance(Lab5.WHEEL_RAD, -dist), true);
+                    rightMotor.rotate(convertDistance(Lab5.WHEEL_RAD, -dist), false);
+                    //navigation.travelTo(LLx, LLy);
+                    navigation.turnTo(angle+10);
+                }
+            }
+            if(Math.abs(odometer.getT()-90) <= 8) {
+                leftMotor.stop(true);
+                rightMotor.stop();
+                notFinished = false;
+            }
+        }
+	}
+	
+	public void findObject(double distance, int TB) {
+		//leftMotor.rotate(convertDistance(Lab5.WHEEL_RAD, distance), true);
+		//rightMotor.rotate(convertDistance(Lab5.WHEEL_RAD, distance), true);
+		leftMotor.forward();
+		rightMotor.forward();
+		while(true) {
+			if(getDistance() < 3.6) {
+				leftMotor.stop(true);
+				rightMotor.stop();
+				break;
+			}
+		}
+		int color = getColor();
+		if(color == TB) {
+			LCD.drawString("Object Detected", 0, 0);
+			//LCD.drawString("Red", 0, 1);
+			Sound.beep();
+		}
+		else {
+			Sound.twoBeeps();
+		}
+	}
+	public int getColor() {
+		sampleProvider = cSensor.getRGBMode();
+	    sampleSize = sampleProvider.sampleSize();
+	    float []colorSample = new float[sampleSize];
+		sampleProvider.fetchSample(colorSample, 0);
+		if(ifRed(colorSample[0], colorSample[1], colorSample[2])) {
+			return 1;
+		}
+		if(ifBlue(colorSample[0], colorSample[1], colorSample[2])) {
+			return 2;
+		}
+		if(ifYellow(colorSample[0], colorSample[1], colorSample[2])) {
+			return 3;
+		}
+		if(ifWhite(colorSample[0], colorSample[1], colorSample[2])) {
+			return 4;
+		}
+		return 0;
+	}
+	public boolean ifRed(float R, float G, float B) {
+		double dr = redMeanR - R*100;
+		double dg = redMeanG - G*100;
+		double db = redMeanB - B*100;
+		double d = Math.sqrt(dr*dr + dg*dg + db*db);
+		if(d < thresholdRed) {
+			return true;
+		}
+		return false;
+	}
+	public boolean ifBlue(float R, float G, float B) {
+		double dr = blueMeanR - R*100;
+		double dg = blueMeanG - G*100;
+		double db = blueMeanB - B*100;
+		double d = Math.sqrt(dr*dr + dg*dg + db*db);
+		if(d < thresholdRed) {
+			return true;
+		}
+		return false;
+	}
+	public boolean ifYellow(float R, float G, float B) {
+		double dr = yellowMeanR - R*100;
+		double dg = yellowMeanG - G*100;
+		double db = yellowMeanB - B*100;
+		double d = Math.sqrt(dr*dr + dg*dg + db*db);
+		if(d < thresholdRed) {
+			return true;
+		}
+		return false;
+	}
+	public boolean ifWhite(float R, float G, float B) {
+		double dr = whiteMeanR - R*100;
+		double dg = whiteMeanG - G*100;
+		double db = whiteMeanB - B*100;
+		double d = Math.sqrt(dr*dr + dg*dg + db*db);
+		if(d < thresholdRed) {
+			return true;
+		}
+		return false;
+	}
+	public double getDistance() {
+		SampleProvider sampleProvider = usSensor.getMode("Distance"); // usDistance provides samples from
+        // this instance
+		float[] usDistance = new float[3];
+		List<Double> distance = new ArrayList<Double>();
+		//sampleProvider.fetchSample(usDistance, 0);
+		for(int i = 0; i < 9; i++) {
+			sampleProvider.fetchSample(usDistance, 0);
+			LCD.drawString("us Distance: " + usDistance[0], 0, 5);
+			//distance[i] = usDistance[0]*100;
+			distance.add((double)usDistance[0]*100);
+			Delay.msDelay(30);
+		}
+		Collections.sort(distance);
+		return distance.get(9/2);
+	}
+	
+	private static int convertDistance(double radius, double distance) {
+		return (int) ((180.0 * distance) / (Math.PI * radius));
+	}
+	private static int convertAngle(double radius, double width, double angle) {
+		return convertDistance(radius, Math.PI * width * angle / 360.0);
+	}	
+}
---
+++ lab5-1.5/src/ca/mcgill/ecse211/lab5/UltrasonicLocalizer.java
@@ -0,0 +1,179 @@
+package ca.mcgill.ecse211.lab5;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import ca.mcgill.ecse211.odometer.Odometer;
+import lejos.hardware.Sound;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
+
+public class UltrasonicLocalizer {
+	public enum LocalizationType{fallingEdge, risingEdge};
+	private EV3LargeRegulatedMotor leftMotor;
+	private EV3LargeRegulatedMotor rightMotor;
+	private EV3UltrasonicSensor usSensor;
+	private Odometer odometer;
+	private LocalizationType type;
+	private static final int FORWARD_SPEED = 200;
+	private static final int ROTATE_SPEED =100;
+	private static final int DETECT_DISTANCE = 45;
+	private double distance[];
+	
+	
+	public UltrasonicLocalizer(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor, LocalizationType type,
+			Odometer odometer, EV3UltrasonicSensor usSensor) {
+		this.leftMotor = leftMotor;
+		this.rightMotor = rightMotor;
+		this.type = type;
+		this.odometer = odometer;
+		this.usSensor = usSensor;
+		
+	}
+	public void UltrasonicLocalization() {
+		double alpha, beta;
+		//rising edge
+		if(type == LocalizationType.risingEdge) {
+			leftMotor.setSpeed(ROTATE_SPEED);
+			rightMotor.setSpeed(ROTATE_SPEED);
+			leftMotor.forward();
+			rightMotor.backward();
+			while(true) {
+				if(getDistance() < DETECT_DISTANCE) {
+					break;
+				}
+			}
+			//rotate to the left until it detects a rising edge
+			while(true) {
+				if(getDistance() > DETECT_DISTANCE) {
+					alpha = odometer.getT();
+					Sound.beep();
+					leftMotor.stop(true);
+					rightMotor.stop();
+					break;
+				}
+			}
+			leftMotor.setSpeed(ROTATE_SPEED);
+			rightMotor.setSpeed(ROTATE_SPEED);
+			leftMotor.backward();
+			rightMotor.forward();
+			Delay.msDelay(1000);
+			sleep(1000);
+			//keep rotating to the left until it sees a wall
+			while(true) {
+				if(getDistance() > DETECT_DISTANCE) {
+					beta = odometer.getT();
+					Sound.beep();
+					leftMotor.stop(true);
+					rightMotor.stop();
+					break;
+				}
+			}
+			if(alpha < beta) {
+				odometer.setTheta((45-(alpha+beta)/2) + odometer.getT());
+			}
+			else {
+				odometer.setTheta((225-(alpha+beta)/2) + odometer.getT());
+			}
+			turnTo(0);			
+		}
+		//falling edge
+		else{
+			leftMotor.setSpeed(ROTATE_SPEED);
+			rightMotor.setSpeed(ROTATE_SPEED);
+			leftMotor.backward();
+			rightMotor.forward();
+			//rotate to the left until it detects a falling edge
+			while(true) {
+				if(getDistance() > DETECT_DISTANCE) {
+					break;
+				}
+			}
+			while(true) {
+				if(getDistance() < DETECT_DISTANCE) {
+					alpha = odometer.getT();
+					Sound.beep();
+					leftMotor.stop(true);
+					rightMotor.stop();
+					break;
+				}
+			}
+			leftMotor.setSpeed(ROTATE_SPEED);
+			rightMotor.setSpeed(ROTATE_SPEED);
+			leftMotor.forward();
+			rightMotor.backward();
+			Delay.msDelay(1000);
+			sleep(1000);
+			//keep rotating to the left until it sees a wall
+			while(true) {
+				if(getDistance() < DETECT_DISTANCE) {
+					beta = odometer.getT();
+					Sound.beep();
+					leftMotor.stop(true);
+					rightMotor.stop();
+					break;
+				}
+			}
+			if(alpha < beta) {
+				odometer.setTheta((45-(alpha+beta)/2) + odometer.getT());
+			}
+			else {
+				odometer.setTheta((225-(alpha+beta)/2) + odometer.getT());
+			}
+			turnTo(0);			
+		}
+		
+	}
+	public void turnTo(double theta) {
+		double turnAngle;
+		turnAngle = theta - odometer.getT();
+		//calculate the minimal angle
+		if(turnAngle < -180) {
+			turnAngle = turnAngle + 360;
+		}
+		else if(turnAngle > 180) {
+			turnAngle = turnAngle - 360;
+		}
+		leftMotor.setSpeed(ROTATE_SPEED);
+		rightMotor.setSpeed(ROTATE_SPEED);
+		leftMotor.rotate(convertAngle(Lab5.WHEEL_RAD, Lab5.TRACK, turnAngle), true);
+		rightMotor.rotate(-convertAngle(Lab5.WHEEL_RAD, Lab5.TRACK, turnAngle), false);
+	}
+	
+	private static int convertAngle(double radius, double width, double angle) {
+		return convertDistance(radius, Math.PI * width * angle / 360.0);
+	}
+	private static int convertDistance(double radius, double distance) {
+		return (int) ((180.0 * distance) / (Math.PI * radius));
+	}
+	
+	public double getDistance() {
+		SampleProvider sampleProvider = usSensor.getMode("Distance"); // usDistance provides samples from
+        // this instance
+		float[] usDistance = new float[3];
+		List<Double> distance = new ArrayList<Double>();
+		//sampleProvider.fetchSample(usDistance, 0);
+		for(int i = 0; i < 5; i++) {
+			sampleProvider.fetchSample(usDistance, 0);
+			LCD.drawString("us Distance: " + usDistance[0], 0, 5);
+			//distance[i] = usDistance[0]*100;
+			distance.add((double)usDistance[0]*100);
+			Delay.msDelay(30);
+		}
+		Collections.sort(distance);
+		return distance.get(5/2);
+	}
+	public static void sleep(int time) {
+		long a = System.currentTimeMillis();
+		long b = System.currentTimeMillis();
+		while( (b-a) <= time) {
+			b = System.currentTimeMillis();
+		}
+	}
+}
